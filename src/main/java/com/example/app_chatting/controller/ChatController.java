a20696f51b8cfe3282f6dfabf873d3b3581c19ff
JO YEJI <whdpwl2@gmail.com>
2022-12-19 10:52:58 +0900

    Feat: message 작성 request 만들기

--- src/main/java/com/example/app_chatting/controller/ChatController.java
+++ src/main/java/com/example/app_chatting/controller/ChatController.java
@@ -5,10 +5,7 @@ import com.example.app_chatting.domain.RsData;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,14 +14,24 @@ import java.util.List;
 @RequestMapping("/chat")
 public class ChatController {
     private List<ChatMessage> messageList = new ArrayList<>();
+
+    //post response dto
     @AllArgsConstructor
     @Getter
     public static class WriteMessageResponse{
         private final Long id;
     }
+    //post request dto
+    @AllArgsConstructor
+    @Getter
+    public static class WriteMessageRequest{
+        private final String authorName;
+        private final String content;
+    }
+
     @PostMapping("/messages")
-    public RsData<WriteMessageResponse> writeMessage(){
-        ChatMessage message = new ChatMessage("hoon", "hi");
+    public RsData<WriteMessageResponse> writeMessage(@RequestBody WriteMessageRequest request){
+        ChatMessage message = new ChatMessage(request.getAuthorName(), request.getContent());
         messageList.add(message);
         return new RsData<>("S-1", "메세지가 작성되었습니다.", new WriteMessageResponse(message.getId()));
     }
