69393171a35a58eeea83137a85daac9a4370010a
JO YEJI <whdpwl2@gmail.com>
2022-12-19 11:31:42 +0900

    Feat: message 목록 받아오는 api 만들기 requestParam으로 fromId를 주면 해당 아이디 이후의 메세지들만 가져온다.

--- src/main/java/com/example/app_chatting/controller/ChatController.java
+++ src/main/java/com/example/app_chatting/controller/ChatController.java
@@ -4,6 +4,7 @@ import com.example.app_chatting.domain.ChatMessage;
 import com.example.app_chatting.domain.RsData;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/chat")
+@Slf4j
 public class ChatController {
     private List<ChatMessage> messageList = new ArrayList<>();
 
@@ -36,7 +38,19 @@ public class ChatController {
         return new RsData<>("S-1", "메세지가 작성되었습니다.", new WriteMessageResponse(message.getId()));
     }
     @GetMapping("/messages")
-    public RsData<List<ChatMessage>> getMessages(){
-        return new RsData<>("S-1", "성공했습니다.", messageList);
+    public RsData<List<ChatMessage>> getMessages(@RequestParam(required = false) Long fromId){
+        log.info("fromId : {}", fromId);
+        long idx = -1;
+        if (fromId != null){
+            for (long i = messageList.size()-1; i >= 0; i--){
+                if (messageList.get((int) i).getId() <= fromId){
+                    idx = i;
+                    break;
+                }
+            }
+        } else {
+            return new RsData<>("S-1", "성공했습니다.", messageList);
+        }
+        return new RsData<>("S-1", "성공했습니다.", messageList.subList((int) (idx+1), messageList.size()));
     }
 }
